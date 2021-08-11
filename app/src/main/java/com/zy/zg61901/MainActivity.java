eca6073ce1bbf70b32d0e9ba299b34731f18e1a3
hahajing <444511958@qq.com>
2021-08-11 09:38:04 +0800

    test remote branch dev

--- app/src/main/java/com/zy/zg61901/MainActivity.java
+++ app/src/main/java/com/zy/zg61901/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("123", "onCreate: 111");
+        Log.d("123", "onCreate: 2222");
     }
 }
